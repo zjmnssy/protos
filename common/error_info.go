57a21645c211d850aad746eb6fba25048dc8e4a3
nssy <624029344@qq.com>
2020-04-07 20:32:08 +0800

    add error info

---
+++ common/error_info.go
@@ -0,0 +1,26 @@
+package common
+
+import fmt "fmt"
+
+// ErrorMsg 错误提示信息表
+var ErrorMsg = make(map[ErrorCode]string)
+
+// InitErrorMsg 初始化错误信息表
+func InitErrorMsg() {
+	for k := range ErrorCode_name {
+		ErrorMsg[ErrorCode(k)] = fmt.Sprintf("内部错误(%d)", k)
+	}
+
+	ErrorMsg[ErrorCode_Success] = "success"
+
+	ErrorMsg[ErrorCode_WebSuccess] = "success"
+	ErrorMsg[ErrorCode_WebMethodError] = "method error"
+	ErrorMsg[ErrorCode_WebRouteNotFound] = "route not found"
+	ErrorMsg[ErrorCode_WebCookieOntFound] = "cookie missing"
+	ErrorMsg[ErrorCode_WebCookieIllegal] = "cookie illegal"
+
+	ErrorMsg[ErrorCode_NetworkCommonError] = "network error"
+	ErrorMsg[ErrorCode_NetworkDeadline] = "network deadline"
+	ErrorMsg[ErrorCode_NetworkConnectionRefused] = "connection refused"
+	ErrorMsg[ErrorCode_NetworkDestUnreachable] = "dest unreachable"
+}
